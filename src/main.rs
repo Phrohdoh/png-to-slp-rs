553b16352299b8069f8b3225a6218b70887d3574
Taryn Hill <taryn@phrohdoh.com>
2017-05-14 09:30:58 -0500

    Initial commit of png-to-slp

---
+++ src/main.rs
@@ -0,0 +1,31 @@
+use std::fs::File;
+
+extern crate clap;
+use clap::{App, Arg};
+
+extern crate chariot_slp;
+
+extern crate png;
+
+fn main() {
+    let matches = App::new("png-to-slp")
+        .version("0.1.0")
+        .author("Taryn Hill <taryn@phrohdoh.com>")
+        .about("Convert a PNG file to an SLP")
+        .arg(Arg::with_name("png-path")
+            .long("png-path")
+            .value_name("/path/to/indexed.png")
+            .help("Filepath to the PNG to convert to an SLP")
+            .required(true)
+            .takes_value(true))
+        .get_matches();
+
+    let png_path = matches.value_of("png-path").unwrap();
+    let f = File::open(png_path).expect(&format!("Failed to open {}", &png_path));
+    let decoder = png::Decoder::new(f);
+    let (info, mut reader) = decoder.read_info().expect("Failed to 'read_info' ???");
+    let mut buf = vec![0; info.buffer_size()];
+    reader.next_frame(&mut buf).expect("Failed to read frame");
+
+    assert_eq!(png::ColorType::Indexed, info.color_type);
+}

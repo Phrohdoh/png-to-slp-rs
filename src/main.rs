81f6508c18f7b522581eefafa1c095a8991e7772
Taryn Hill <taryn@phrohdoh.com>
2017-05-14 23:35:52 -0500

    Write out a partially complete SlpShapeHeader for each shape

--- src/main.rs
+++ src/main.rs
@@ -29,8 +29,38 @@ fn main() {
     decoder.set(png::TRANSFORM_IDENTITY);
 
     let (info, mut reader) = decoder.read_info().expect("Failed to 'read_info' ???");
-    let mut buf = vec![0; info.buffer_size()];
-    reader.next_frame(&mut buf).expect("Failed to read frame");
-
     assert_eq!(png::ColorType::Indexed, info.color_type);
+
+    let mut indexed_image_data_buf = vec![0; info.buffer_size()];
+    reader.next_frame(&mut indexed_image_data_buf).expect("Failed to read frame");
+
+    let mut slp_header = chariot_slp::SlpHeader::new();
+    slp_header.file_version = *b"2.0N";
+    slp_header.shape_count = 1;
+    slp_header.comment = *b"Chariot/png-to-slp\0\0\0\0\0\0";
+
+    let mut slp = chariot_slp::SlpFile::new(1u8);
+    slp.header = slp_header;
+
+    let mut output = {
+        let raw_bytes = Vec::new();
+        std::io::Cursor::new(raw_bytes)
+    };
+
+    for i in 0..slp.header.shape_count {
+        let shape_header = chariot_slp::SlpShapeHeader {
+            shape_data_offsets: 32 + 32 * slp.header.shape_count,
+            shape_outline_offset: 0u32,
+            palette_offset: 0u32,
+            properties: 0u32,
+            width: info.width,
+            height: info.height,
+            center_x: (info.width / 2) as i32,
+            center_y: (info.height / 2) as i32,
+        };
+
+        shape_header.write_to(&mut output).expect(&format!("Failed to write out SlpShapeHeader {}: {:?}", i, shape_header));
+    }
+
+    println!("{:?}", output.into_inner());
 }
